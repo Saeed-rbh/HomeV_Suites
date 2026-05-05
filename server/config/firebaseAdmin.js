d1781cf915f5e933f8882a2dbbc35e06b88d4241
Saeed Arabha <saeedarabha@outlook.com>
2026-05-05 18:11:29 -0400

    Update firebase init to use env var

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,9 +1,18 @@
 const admin = require('firebase-admin');
 const path = require('path');
 
-const serviceAccount = require('./firebase-service-account.json');
+let serviceAccount;
+try {
+    serviceAccount = require('./firebase-service-account.json');
+} catch (error) {
+    if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+        serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    } else {
+        console.error('Missing Firebase Service Account Credentials. Please set FIREBASE_SERVICE_ACCOUNT env var.');
+    }
+}
 
-if (!admin.apps.length) {
+if (!admin.apps.length && serviceAccount) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount)
     });
